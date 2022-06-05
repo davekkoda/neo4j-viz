0e2a91a9dd971b654113da629f385416f7b94e2a
davekkoda <david_nguyen@hakkoda.io>
2022-06-04 23:31:39 -0400

    Idk how to connect to neo4j.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useReadCypher } from 'use-neo4j'
 
 function App() {
+  const { cypher, error, loading, first } = useReadCypher('MATCH (n) RETURN count(n) AS count')
+
+  // Default to Loading Message
+  let result = (<div className="ui active dimmer">Loading...</div>)
+
+  // Was there an error om the query?
+  if ( error ) {
+    result = (<div className="ui negative message">{ error.message }</div>)
+  }
+  else if ( !loading ) {
+    // Get the count
+    const count = first?.get('count').toNumber()
+    result = (<div>There are {count} nodes in the database.</div>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{cypher}</pre>
+      {result}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Neo4jProvider } from 'use-neo4j';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const logo = () => {
+  return (<div className="logo">Hello</div>)
+}
+
+const footer = () => {
+  return (<div className="footer">
+    <a href="https://github.com/adam-cowley/graphapp-starter-react" rel="noopener noreferrer" target="_blank">
+      </a>
+    </div>)
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Neo4jProvider logo={logo()} footer={footer()}>
+      <App />
+    </Neo4jProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
